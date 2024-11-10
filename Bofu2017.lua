d4cb75d9dec2920c9d31cc80a55b2474054d40b8
Reapvitalized <realvitalization@proton.me>
2024-11-10 10:40:12 +0800

    Update Bofu2017.lua

--- Bofu2017.lua
+++ Bofu2017.lua
@@ -1,1 +1,10 @@
-false
+whitelist = {
+False=true,True=false,Elseif=false,
+roblox_b2s_FFFFFFFF={False=nil,Do=nil,},
+False=true,True=false,If={False=nil,Do=nil,},
+True=false,False=nil,While={False=nil,Do=nil,',',},
+False=true,True=false,While={True='True',Do='end',},
+False=true,Real=false,While={Real=true,Do=nil,},
+ACTUALWS=true,True=false,
+While={False='nil',
+Do=nil,},}
