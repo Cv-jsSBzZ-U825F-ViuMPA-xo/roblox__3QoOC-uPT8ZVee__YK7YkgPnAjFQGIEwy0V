3730f31bf6ea7537b53aefb639f07a46e74f8d59
Reapvitalized <realvitalization@proton.me>
2024-12-01 09:13:33 +0800

    Create mariosex.lua

---
+++ mariosex.lua
@@ -0,0 +1,13 @@
+local tab = {
+WHITELIST = true,
+Exceptnot = false,
+B = true,
+Exclude = true,
+Blacklisted = 'F',
+E = false,
+R = true,
+T = false,
+Y = true
+rob__earlyaccess_lua = true
+hirandomhttprequestloggers = true
+}
