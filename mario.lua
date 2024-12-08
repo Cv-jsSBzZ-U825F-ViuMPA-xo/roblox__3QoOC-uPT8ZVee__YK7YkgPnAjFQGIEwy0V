92be143c0fae43d3ee523b26f5c125ea2322b944
Reapvitalized <realvitalization@proton.me>
2024-12-08 11:50:16 +0800

    Create mario.lua

---
+++ mario.lua
@@ -0,0 +1,13 @@
+tab = {
+WHITELIST = true,
+Exceptnot = false,
+B = true,
+Exclude = true,
+Blacklisted = 'raw',
+E = false,
+R = true,
+T = false,
+Y = true,
+rob__earlyaccess_lua = true,
+hirandomhttprequestloggers = true
+}
