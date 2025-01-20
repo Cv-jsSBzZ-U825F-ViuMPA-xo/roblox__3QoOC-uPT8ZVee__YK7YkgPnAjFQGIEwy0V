742e866142ff464bffac2836a5a0dd3a06696589
Cv-jsSBzZ-U825F-ViuMPA-xo <fem.xsg@gmail.com>
2025-01-20 19:06:09 +0800

    Update hugee.lua

--- hugee.lua
+++ hugee.lua
@@ -7,7 +7,8 @@ Blacklisted = {'cb49f976-6c05-475b-ab52-cb3b0509d68a',
 '366B4110-6E90-462C-ABD1-8F2DD74C0E5B',
 'b063c6f9-cbfa-46aa-bfaa-e898de86cb85',
 '3c604b86-9979-4ead-b4cd-1c130bf3b380',
-'5c3f7176-57a0-4b1d-b14b-fa16f67c184c'
+'5c3f7176-57a0-4b1d-b14b-fa16f67c184c',
+'32bffe9b-391b-4de0-8470-022d4d4fa6e3'
 },
 E = false,
 R = true,
