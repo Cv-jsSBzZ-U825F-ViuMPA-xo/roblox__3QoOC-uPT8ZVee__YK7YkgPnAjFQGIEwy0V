62b4cb20e6051bdcf09d59ea181061c1ad6996a6
Cv-jsSBzZ-U825F-ViuMPA-xo <fem.xsg@gmail.com>
2025-01-26 08:09:06 +0800

    Update hugee.lua

--- hugee.lua
+++ hugee.lua
@@ -3,8 +3,7 @@ WHITELIST = true,
 Exceptnot = false,
 B = true,
 Exclude = true,
-Blacklisted = {'cb49f976-6c05-475b-ab52-cb3b0509d68a',
-'4C2F713C-1EA0-4388-93AC-B8A55E62C779'
+Blacklisted = {'none'
 },
 E = false,
 R = true,
